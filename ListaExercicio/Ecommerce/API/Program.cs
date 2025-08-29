125abaef1b434e0d727271e5f46d157e49396344
LUIZ FELIPE <luiz.felipe06@cs.up.edu.br>
2025-08-29 11:31:12 -0300

    test HTTP

--- ListaExercicio/Ecommerce/API/Program.cs
+++ ListaExercicio/Ecommerce/API/Program.cs
@@ -2,5 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/oi", () => "olá!");
+app.MapPost("/oi",()=>"Funcionalidade com POST");
 
 app.Run();
