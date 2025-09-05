f879e469e10159d922c6db1346860611fb7ea876
LUIZ FELIPE <luiz.felipe06@cs.up.edu.br>
2025-09-05 11:26:33 -0300

    Listar e cadastrar produtos

---
+++ ListaExercicio/Ecommerce/API/Models/Produto.cs
@@ -0,0 +1,36 @@
+namespace API.Models;
+
+public class Produto
+{
+    // Caracteristicas EM JAVA
+    // id, nome, preco ,quantidade
+    //private string id;
+    //private string nome;
+    //private double preco;
+    //private int quantidade;
+    public string Id { get; set; }
+    public string Nome { get; set; }
+    public int Quantidade { get; set; }
+    public double Preco { get; set; }
+    public DateTime CriadoEm { get; set; }
+
+    public Produto()
+    {
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+
+    // getter e setter em JAVA
+
+    //public string getNome()
+    //{
+    //    return "Nome: " + nome;
+    //}
+    //public void setNome(string nome)
+    //{
+    //   this.nome = nome + " " + DateTime.Now;
+    //}
+
+    // getter e setter C#
+   // public string Nome { get; set; }
+}
--- ListaExercicio/Ecommerce/API/Program.cs
+++ ListaExercicio/Ecommerce/API/Program.cs
@@ -1,8 +1,44 @@
+using API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/oi", () => "olá!");
-app.MapPost("/oi",()=>"Funcionalidade com POST");
+List<Produto> produtos = new List<Produto>
+{
+    new Produto { Nome = "Notebook", Quantidade = 2, Preco = 3500.00 },
+    new Produto { Nome = "Mouse", Quantidade = 10, Preco = 150.50 },
+    new Produto { Nome = "Teclado", Quantidade = 5, Preco = 200.00 },
+    new Produto { Nome = "Monitor", Quantidade = 3, Preco = 1200.00 },
+    new Produto { Nome = "Impressora", Quantidade = 1, Preco = 800.00 }
+};
+
+// GET: Recupera dados de um servidor sem modificar o recurso
+// POST: Envia dados ao servidor para criar um novo recurso;
+// PUT: Atualiza completamente um recurso existente no servidor
+// PATCH: Atualiza parcialmente um recurso existente;
+// DELETE: Remove um recurso do servidor
+
+app.MapGet("/", () => "Página principal");
+app.MapGet("/api/produto/listar", () =>
+{
+    return produtos;
+});
+
+app.MapPost("/api/produto/cadastrar", (Produto produto)=>
+{
+    produtos.Add(produto);   
+});
+
 
 app.Run();
+
+// JAVA
+//Produto produto = new Produto();
+//produto.setNome("Bolacha");
+//Console.WriteLine(produto.getNome());
+
+// C#
+//Produto produto = new Produto();
+//produto.Nome = "Bolacha";
+//Console.WriteLine(produto.Nome);
+
