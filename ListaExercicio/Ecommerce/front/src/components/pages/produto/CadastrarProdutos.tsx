872ab78558286cf9bd781af4e3184e9ca3b80cfe
LUIZ FELIPE <luiz.felipe06@cs.up.edu.br>
2025-10-31 09:42:26 -0300

    Cadastrar e listar produto com fetch

--- ListaExercicio/Ecommerce/front/src/components/pages/produto/CadastrarProdutos.tsx
+++ ListaExercicio/Ecommerce/front/src/components/pages/produto/CadastrarProdutos.tsx
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 import Produto from '../../../models/Produto';
 
 function CadastrarProduto() {
+
     const [nome,setNome] = useState("");
+    const [descricao,setDescricao] = useState("");
+    const [quantidade,setQuantidade] = useState(0);
+    const [preco,setPreco] = useState(0);
+
     function enviarProduto(event: any){
         event.preventDefault(); //evita o recarregamento da pagina
     }
@@ -11,19 +16,28 @@ function CadastrarProduto() {
         setNome(event.target.value)
     }
     async function submeterProdutoAPI(){
-        const produto : Produto = {
-            nome: nome,
-            descricao: "Teste",
-            preco: 1234,
-            quantidade: 2
+
+        try {
+            const produto : Produto = {
+                nome: nome,
+                descricao: descricao,
+                preco: preco,
+                quantidade: quantidade
+            }
+            const resposta = await fetch("http://localhost:5087/api/produto/cadastrar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(produto)
+            });
+            if(!resposta.ok) {
+                throw new Error("Erro na requisição: " + resposta.statusText);
+            }
+            console.log(await resposta.json());
+        } catch (error) {
+            console.error("Erro no cadastro do produto: "+error);
         }
-        const resposta = await fetch("http://localhost:5087/api/produto/cadastrar", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(produto)
-        });
     }
 
            
@@ -37,7 +51,15 @@ function CadastrarProduto() {
                 </div>
                 <div>
                     <label>Descrição</label>
-                    <input type="text" />
+                    <input type="text" onChange={(e : any)=>{
+                        setDescricao(e.target.value);
+                    }}/>
+                </div>
+                <div>
+                    <label>Preço</label>
+                    <input type="text" onChange={(e : any)=>{
+                        setPreco(Number(e.target.value));
+                    }}/>
                 </div>
                 <div>
                     <button type="submit">Cadastrar</button>
